18eee254ec666a0ae4e95eebe1e7f5a3b0daa82b
Kevin McPhillips <github@kevinmcphillips.ca>
2011-06-18 08:38:53 -0500

    Removing and previewing images works and is genericized.

--- lib/attached_image.rb
+++ lib/attached_image.rb
@@ -11,6 +11,10 @@ module AttachedImage
         :path => ":rails_root/public/assets/images/:class/:id_:style.:extension",
         :url => "/assets/images/:class/:id_:style.:extension"
 
+      attr_accessor :delete_image
+
+      before_validation :maybe_delete_image
+
       validates_attachment_size :image, :in => 1..6.megabytes
       validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/pjpeg", "image/png", "image/tiff", "image/x-png", "image/gif"]
 
@@ -27,5 +31,13 @@ module AttachedImage
   end
 
   protected
+
+  def delete_image?
+    delete_image && !delete_image.to_i.zero?
+  end
+
+  def maybe_delete_image
+    self.image = nil if delete_image?
+  end
 
 end
