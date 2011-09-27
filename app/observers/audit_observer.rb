0df1bfe11dc97fb625fcabcb71d1c6c5b175ac6c
Kevin McPhillips <github@kevinmcphillips.ca>
2011-09-26 23:03:43 -0500

    Remove debugging in audit observer

--- app/observers/audit_observer.rb
+++ app/observers/audit_observer.rb
@@ -2,13 +2,10 @@ class AuditObserver < ActiveRecord::Observer
   observe :post, :event, :member, :picture, :block, :link, :executive
 
   def after_save(object)
+    # Do not audit Authlogic updates. These happen on logins and password changes, among other things.
     unless object.is_a?(Member) && (object.changes.keys - ["last_request_at", "crypted_password", "updated_at", "password_salt", "current_login_at", "persistence_token", "login_count", "last_login_at", "password_configured"]).size == 0
-      puts "******************** #{object.changes.keys.inspect}"
-
       audit = Audit.new :auditable => object, :action => (object.new_record? ? "create" : "update"), :member => MemberTracker.current, :details => object.changes
       audit.save
-    else
-      puts "********* Not auditing user activity"
     end
   end
 
