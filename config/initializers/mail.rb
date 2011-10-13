1840fb221d61f0da267312c66d3a20021ad8b9a6
Kevin McPhillips <github@kevinmcphillips.ca>
2011-10-13 17:55:00 -0500

    Simplify mail delivery for production

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,19 +3,23 @@ if File.exists?("#{Rails.root}/config/mail.yml") && !Rails.env.test?
   config = file[Rails.env] if file
 
   if config
-    require 'tlsmail'
-    Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+    if Rails.env.development?
+      require 'tlsmail'
+      Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
 
-    ActionMailer::Base.delivery_method = :smtp
-    ActionMailer::Base.perform_deliveries = true
-    ActionMailer::Base.smtp_settings = config["smtp_settings"].symbolize_keys || {}
-    ActionMailer::Base.default_url_options[:host] = config["host"]
+      ActionMailer::Base.delivery_method = :smtp
+      ActionMailer::Base.perform_deliveries = true
+      ActionMailer::Base.smtp_settings = config["smtp_settings"].symbolize_keys || {}
+      ActionMailer::Base.default_url_options[:host] = config["host"]
 
-    (config["register_interceptors"] || []).each do |i|
-      ActionMailer::Base.register_interceptor i
+      (config["register_interceptors"] || []).each do |i|
+        ActionMailer::Base.register_interceptor i
+      end
+
+      BypassMailInterceptor.email = config["bypass"] unless config["bypass"].blank?
+    elsif Rails.env.production?
+      ActionMailer::Base.smtp_settings = config["smtp_settings"].symbolize_keys || {}
     end
-
-    BypassMailInterceptor.email = config["bypass"] unless config["bypass"].blank?
 
   else
     Rails.logger.error "Could not find config for environment '#{Rails.env}' in mail.yml. Email disabled."
