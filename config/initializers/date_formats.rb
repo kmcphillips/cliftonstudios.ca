5b8dc6a641647db24fcf2e2d8cb5676aaef1dc12
Kevin McPhillips <github@kevinmcphillips.ca>
2011-11-03 09:02:52 -0500

    Correct formatting for long text fields and dates on sidebar

--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -2,6 +2,7 @@ Time::DATE_FORMATS[:without_time] = lambda{ |time| time.strftime("%B #{time.day.
 Time::DATE_FORMATS[:without_year] = lambda{ |time| time.strftime("%B #{time.day.ordinalize}") }
 Time::DATE_FORMATS[:with_time] = lambda{ |time| time.strftime("%B #{time.day.ordinalize} %Y at %I:%M%p") }
 Time::DATE_FORMATS[:with_time_no_minutes] = lambda{ |time| hour = time.strftime("%I").gsub(/^0/, ""); time.strftime("%B #{time.day.ordinalize} %Y at #{hour} %p") }
+Time::DATE_FORMATS[:with_time_condensed] = lambda{ |time| time.strftime("%b #{time.day.ordinalize}, %I:%M%p") }
 Time::DATE_FORMATS[:index] = "%b %e/%Y %I:%M%p"
 Time::DATE_FORMATS[:filename] = "%d-%m-%Y"
 Time::DATE_FORMATS[:form] = "%d/%m/%Y %I:%m %p"
