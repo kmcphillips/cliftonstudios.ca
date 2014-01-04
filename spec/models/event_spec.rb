aae90c7a40aa750638d3fafa5e810fad124157f6
Kevin McPhillips <github@kevinmcphillips.ca>
2014-01-04 17:09:51 -0600

    Use time zones for when app config and system do not have the same time zone

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe Event do
   before(:each) do
     @member = mock_model(Member)
-    @valid_attributes = {:title => "title", :member => @member, :body => "body", :starts_at => Time.now, :duration => 1}
+    @valid_attributes = {:title => "title", :member => @member, :body => "body", :starts_at => Time.zone.now, :duration => 1}
   end
   
   describe "ends_at" do
     it "should be set on save" do
       event = Event.create!(@valid_attributes)
-      event.ends_at.should == (Time.now.beginning_of_day + 1.day)
+      event.ends_at.should == (Time.zone.now.beginning_of_day + 1.day)
     end
   end
  
