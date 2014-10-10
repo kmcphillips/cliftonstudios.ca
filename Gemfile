4920033c797eaa1aa3dbc783491e60c097c6dd0e
Kevin McPhillips <github@kevinmcphillips.ca>
2014-10-10 18:56:23 -0400

    Use jquery ui gem

--- Gemfile
+++ Gemfile
@@ -28,6 +28,7 @@ gem 'sass-rails'
 gem 'uglifier'
 gem 'coffee-rails'
 gem 'jquery-rails'
+gem 'jquery-ui-rails'
 gem 'fancybox-rails'
 
 group :development do
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
 //= require jquery
 //= require jquery_ujs
+//= require jquery-ui
 //= require fancybox
-//= require vendor/jquery-ui
 //= require initializers
