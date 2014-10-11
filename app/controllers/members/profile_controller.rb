7fc1d5ce658202d7d6ae0e565182eeb782c8b7d7
Kevin McPhillips <github@kevinmcphillips.ca>
2014-10-10 22:41:16 -0400

    Strong params

--- app/controllers/members/profile_controller.rb
+++ app/controllers/members/profile_controller.rb
@@ -5,7 +5,7 @@ class Members::ProfileController < ApplicationController
   end
 
   def create # actually update for current_user
-    if current_member.update_attributes(params[:member])
+    if current_member.update_attributes(member_params)
       if current_member.profile_configured?
         flash[:notice] = "Your profile has been updated but it is still missing a few things. You can fix it later."
       else
@@ -16,5 +16,11 @@ class Members::ProfileController < ApplicationController
     else
       render :index
     end
+  end
+
+  private
+
+  def member_params
+    params.require(:member).permit(:name, :email, :address, :phone, :alternate_phone, :receive_emails, :contact_method)
   end
 end
