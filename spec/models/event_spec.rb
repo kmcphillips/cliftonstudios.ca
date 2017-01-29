80b9579db8e8e855d067ab928d78e4b97fa04fd5
Kevin McPhillips <github@kevinmcphillips.ca>
2017-01-28 23:38:29 -0500

    Refactor and expand event test

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -16,24 +16,55 @@ describe Event do
   end
 
   describe "class method" do
-    before(:each) do
-      Timecop.freeze(Date.parse("01/09/2012").to_time + 20.minutes)
-    end
+    let(:in_2012) { Date.parse("01/09/2012").to_time + 20.minutes }
+    let(:in_2016) { Date.parse("01/01/2016").to_time }
 
     describe "#next_two_meetings" do
-      it "should calculate the next two meetings" do
-        expect(Event.next_two_meetings).to eq([Date.parse("02/10/2012"), Date.parse("02/04/2013")])
+      shared_context "next_two_meetings for" do
+        it "should calculate the next two meetings" do
+          Timecop.travel(current_time) do
+            expect(Event.next_two_meetings).to eq(expected_result)
+          end
+        end
+      end
+
+      context "2012" do
+        let(:current_time) { in_2012 }
+        let(:expected_result) { [Date.parse("02/10/2012"), Date.parse("02/04/2013")] }
+
+        include_context "next_two_meetings for"
+      end
+
+      context "2016" do
+        let(:current_time) { in_2016 }
+        let(:expected_result) { [Date.parse("05/04/2016"), Date.parse("04/10/2016")] }
+
+        include_context "next_two_meetings for"
       end
     end
 
     describe "#next_two_sales" do
-      it "should calculate the next two sales" do
-        expect(Event.next_two_sales).to eq([[Date.parse("24/11/2012"), Date.parse("25/11/2012")], Date.parse("04/05/2013")])
+      shared_context "next_two_sales for" do
+        it "should calculate the next two sales after" do
+          Timecop.travel(current_time) do
+            expect(Event.next_two_sales).to eq(expected_result)
+          end
+        end
       end
-    end
 
-    after(:each) do
-      Timecop.return
+      context "2012" do
+        let(:current_time) { in_2012 }
+        let(:expected_result) { [[Date.parse("24/11/2012"), Date.parse("25/11/2012")], Date.parse("04/05/2013")] }
+
+        include_context "next_two_sales for"
+      end
+
+      context "2016" do
+        let(:current_time) { in_2016 }
+        let(:expected_result) { [Date.parse("30/04/2016"), [Date.parse("26/11/2016"), Date.parse("27/11/2016")]] }
+
+        include_context "next_two_sales for"
+      end
     end
   end
 end
