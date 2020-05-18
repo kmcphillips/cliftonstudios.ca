5a7b4f09e5b116155472521d05722db4b486d24a
Kevin McPhillips <github@kevinmcphillips.ca>
2020-05-18 16:15:57 -0400

    remove mailgun key

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,12 +66,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = {
-    api_key: secrets.mailgun_api_key,
-    domain: 'from.cliftonstudios.ca'
-  }
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.improvmx.com',
